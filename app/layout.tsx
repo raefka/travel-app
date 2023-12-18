87d93caae5670905ca70580776696215cff6e0db
raefka <raefkacem20@gmail.com>
2023-12-18 21:10:45 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next'
+import './globals.css'
+import NavBar from '@/components/NavBar'
+import Footer from '@/components/Footer'
+
+
+
+export const metadata: Metadata = {
+  title: 'Travel',
+  description: 'Travel UI/UX App for Camping',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body >
+        <NavBar />
+        <main className="relative overflow-hidden">
+        {children}
+        </main>
+        <Footer/>
+      </body>
+    </html>
+  )
+}
+
